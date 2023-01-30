d0e188f48e32de6e9344b033d6d524e2e7c93143
Fotios Vaitsopoulos <100092577+Epic-legend128@users.noreply.github.com>
2023-01-30 12:08:05 +0100

    Create Temperatures.cs

---
+++ Easy/Temperatures/Temperatures.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine()); // the number of temperatures to analyse
+        string[] inputs = Console.ReadLine().Split(' ');
+        int mn;
+        if (n == 0) {
+            mn = 0;
+        }
+        else {
+            mn = int.Parse(inputs[0]);// a temperature expressed as an integer ranging from -273 to 5526
+        }
+        for (int i = 1; i < n; i++)
+        {
+            int t = int.Parse(inputs[i]);// a temperature expressed as an integer ranging from -273 to 5526
+            if (t == 0) {
+                mn = 0;
+                break;
+            }
+            else if (Math.Abs(mn) == Math.Abs(t) && mn < 0) {
+                mn = t;
+            }
+            else if (Math.Abs(mn) > Math.Abs(t)) {
+                mn = t;
+            }
+        }
+
+        // Write an answer using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+        
+        Console.WriteLine(mn);
+    }
+}
